a8e69c3eca3eed797f40f78693f58297c47defc8
Сергей <cepega333@yandex.ru>
2021-10-01 21:17:23 +0500

    set connect to database ToDoDatabase

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,8 +1,13 @@
-﻿namespace TodoApi.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Models
 {
     #region snippet
+    [Table("TodoItems")]
     public class TodoItem
     {
+        [Key]
         public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
